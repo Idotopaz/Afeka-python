8f3e9274a542f74a19a9f830bef936fca219eb9a
Idotopaz <32900985+Idotopaz@users.noreply.github.com>
2025-11-12 09:01:09 +0000

    updated weather exercise

--- Class Work/12.11.25/weather.py
+++ Class Work/12.11.25/weather.py
@@ -1,6 +1,10 @@
-num=int(input("enter the weather in degrees "))
+num = int(input("enter the weather in degrees "))
+clouds = str(input("are there clouds today? (yes/no)"))
 
-if num<11:
+if num < 11 and clouds == "yes":
     print("it is going to rain today")
+else:
+    print("no rain today")
 
 print("bye")
+
