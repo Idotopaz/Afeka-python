ed0f30708a02d5dd16dc8ff97c9b0f066c3a19ea
Idotopaz <32900985+Idotopaz@users.noreply.github.com>
2025-11-12 08:13:12 +0000

    fixed

--- Class Work/9.11.25/oopisateNumbers.py
+++ Class Work/9.11.25/oopisateNumbers.py
@@ -8,13 +8,13 @@ num1_dig2=num1//100%10
 num2=int(input("enter a 3 digit number: "))
 
 #num2=462 dig0-2
-num2_dig0=num2%10
-num2_dig1=num2//10%10       
-num2_dig2=num2//100%10
+num2_dig0=num2//100%10  
+num2_dig1=num2//10%10
+num2_dig2=num2%10
 
-print(num1_dig0==num2_dig2 and num1_dig1==num2_dig2 and num1_dig2==num2_dig0 and (num1*num2)%11 == 0)
+print(num1_dig0==num2_dig0 and num1_dig1==num2_dig1 and num1_dig2==num2_dig2 and (num1*num2)%11 == 0)
 
-#fix at home
+
 
 
 
