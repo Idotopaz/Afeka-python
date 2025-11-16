a34a17edc6c6d76b9c5799771ef5ee660b084091
Idotopaz <32900985+Idotopaz@users.noreply.github.com>
2025-11-16 09:28:10 +0000

    1

--- Class Work/16.11.25/daily calories.py
+++ Class Work/16.11.25/daily calories.py
@@ -1,18 +1,14 @@
 age = float(input("how old? "))
+hight = float(input("how tall? in cm: "))
 
-if age<0 or age>8:
+if age<0 or age>8 or hight or hight<40 or hight>140:
     print("invalid input")
     exit()
 
-if age>=0 and age <3:
+if age >= 0 and age < 3:
     calories = 700
-
-elif age>=3 and age <8:
-    hight = float(input("how tall? in cm: "))
-    if hight<40 or hight>140:
-        print("invalid input")
-        exit()
-    elif hight>=40 and hight<=100:
+else:
+    if hight >= 40 and hight <= 100:
         calories = 900
     else:
         calories = 1200
@@ -20,3 +16,6 @@ elif age>=3 and age <8:
 print(f"the kid needs to eat {int(calories)} calories")
 
 
+
+        
+        
