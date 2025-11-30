3266a8bf2ccd8c22e9fe1c5e1b14e2676f0e9dca
idotopazs <idotopaz02@gmail.com>
2025-12-01 00:52:36 +0200

    task 4 verefication tz ready to upload

--- Home Work/Task 4/verification_digit.py
+++ Home Work/Task 4/verification_digit.py
@@ -1,43 +1,42 @@
 tz = int(input("enter tz: "))
 
-bikoret=tz%10
+check_number=tz%10
 tz_temp = tz//10
 tz//=10
 
 temp1=0
-sum=0
+total=0
 round_up=0
-counter1 = 0
-counter2=0
+counter_tz_length=0
+multiplier=0
 
 while tz_temp > 0:
-    counter1 += 1
+    counter_tz_length += 1
     tz_temp //= 10
-if counter1 == 8:
-    counter2 = 2
-    print(f"here {counter1} , {tz}")
+    
+if counter_tz_length == 8:
+    multiplier = 2
 else:
-    print(f"here {counter1} , {tz}")
-    counter2 = 1
+    multiplier = 1
+    
 while tz>0:
-    if counter2==2:
-        temp1=(tz%10)*counter2
-        print(f"{temp1}+",end=" ")
-        counter2//=2
-        if temp1>9:
-            sum += (temp1%10+temp1//10)
-        else:
-            sum+=temp1
-    elif counter2==1:
-        temp1=(tz%10)*counter2
-        counter2*=2
-        print(f"{temp1}+",end=" ")
-        sum+=temp1
+    temp1=(tz%10)*multiplier
+    if temp1>9:
+        total += (temp1%10+temp1//10)
+        #print(f"+{temp1//10}+{temp1%10}", end="")
+    else:
+        total+=temp1
+        #print(f"+{temp1}", end="")
+    if multiplier == 1:
+        multiplier = 2
+    else:
+        multiplier = 1
     tz//=10
-print(f"={sum}")
-round_up = ((sum//10)*10)+10
-bdika=(round_up-sum)==bikoret
-print(bdika)
+
+round_up = ((total//10)*10)+10
+#print(f"={total}")
+#print(round_up-total)
+print(round_up-total==check_number)
 
 
 
