0db2bdd9cacedc018b582107662aa47769e495b7
Idotopaz <32900985+Idotopaz@users.noreply.github.com>
2025-12-02 18:40:41 +0000

    task 5 ex1 done finally!!

--- Home Work/Task 5/einstein.py
+++ Home Work/Task 5/einstein.py
@@ -1,7 +1,12 @@
 
-for i in range(11,100):
-    for j in range (11,100):
-        if (i//10)/(j//10) == i/j and i/j<1:
-            print(f"{(i//10)}/{(j//10)} == {i}/{j}")
 
+# print(17//10,"/",85%10)
+count=0
+for j in range(11,100):
+    for i in range (11,100):
+            if j%10!=0:
+                if (i//10)/(j%10)==i/j and i/j<1:
+                    print(f"{(i//10)}/{(j%10)} == {i}/{j}")
+                    count+=1
+print(count)
 
