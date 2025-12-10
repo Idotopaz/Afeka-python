9e282e40a1bfc42d0f2c6b3ad80531631e32fe48
idotopazs <idotopaz02@gmail.com>
2025-12-10 23:22:34 +0200

    fixed task 6

--- Home Work/Task 6/min_digits_number.py
+++ Home Work/Task 6/min_digits_number.py
@@ -15,10 +15,19 @@ def new_number_from_min_digits(num1, num2):
         total=0
         pose=1
         while num1>0:
-            total+= (min(num1%10,num2%10))*pose
-            pose*=10
+            if num1%10<num2%10:
+                total+=(num1%10)*pose
+            else:
+                total +=  (num2 % 10) * pose
+            pose *= 10
             num1//=10
             num2//=10
         return total
-print(new_number_from_min_digits(194,456))
 
+def main():
+    num1 = int(input("Enter a number: "))
+    num2 = int(input("Enter a number: "))
+    print(new_number_from_min_digits(num1,num2))
+
+if __name__=="__main__":
+    main()
